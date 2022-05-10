4f1e666eef397ba6837af6259f1b96aab937cf7d
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-05-10 10:22:52 +0200

    specified subscription with chatroom id

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -1,6 +1,7 @@
 class ChatroomChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "everyone"
+    @chatroom = Chatroom.find(params[:id])
+    stream_for @chatroom
   end
 
   def unsubscribed
--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -1,12 +1,13 @@
 import { Controller } from "stimulus"
 import consumer from '../channels/consumer'
 export default class extends Controller {
-
+  static values = { chatroomId: Number}
   connect() {
     consumer.subscriptions.create(
-      "ChatroomChannel",
+      {channel: "ChatroomChannel", id: this.chatroomIdValue },
       { received: (message) =>
-        this.element.insertAdjacentHTML('beforeend', message)}
+        this.element.insertAdjacentHTML('beforeend', message)},
+        this.element.scrollTop = this.element.scrollHeight
       )
   }
 }
